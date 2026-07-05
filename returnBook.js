b17325f9bcf37d8945f673ae43d8fe77da232d97
Samarth Sawant <sawant.samarths@gmail.com>
2026-07-05 22:21:48 +0530

    added auto borrowing record to the next person in reserve list once book is returned

--- returnBook.js
+++ returnBook.js
@@ -1,5 +1,6 @@
 const r = require("./readData");
 const w = require("./writeData");
+const b = require("./borrowManager");
 
 function returnBook(memberId, bookId) {
   let memberData = r.read("./data/member/" + memberId + ".json");
@@ -14,6 +15,18 @@ function returnBook(memberId, bookId) {
   w.write("./data/book/" + bookId + ".json", bookData);
   w.write("./data/member/" + memberId + ".json", memberData);
   w.write("./data/borrow/" + memberId + bookId + ".json", borrowData);
+
+  let reserveData = r.read("./data/reserve/" + bookId + ".json");
+  if (reserveData == null) {
+    console.log("No reservations");
+  } else {
+    if (reserveData.length > 0) {
+      let nextId = reserveData[0];
+      reserveData.splice(0);
+      w.write("./data/reserve/" + bookId + ".json", reserveData);
+      b.borrowBook(nextId, bookId);
+    }
+  }
 }
 
 module.exports = { returnBook };
