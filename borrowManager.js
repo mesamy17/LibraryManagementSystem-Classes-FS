402915516cb9a0925803c61a83acbe02a63e1568
Samarth Sawant <sawant.samarths@gmail.com>
2026-07-08 10:15:43 +0530

    Changed input date from machine to user

--- borrowManager.js
+++ borrowManager.js
@@ -3,7 +3,7 @@ const w = require("./writeData");
 const brr = require("./borrowRecord");
 const a = require("./addData");
 
-function borrowBook(memberId, bookId) {
+function borrowBook(memberId, bookId, borrowDate) {
   try {
     let membersData = r.read("./data/member/" + memberId + ".json");
     let booksData = r.read("./data/book/" + bookId + ".json");
@@ -15,11 +15,18 @@ function borrowBook(memberId, bookId) {
         membersData["borrowLimit"] = membersData["borrowLimit"] - 1;
 
         let borrowId = memberId + bookId;
-        let data = new brr(memberId, bookId);
+        let data = new brr(memberId, bookId, borrowDate);
 
         w.write("./data/book/" + bookId + ".json", booksData);
         w.write("./data/member/" + memberId + ".json", membersData);
         w.write("./data/borrow/" + memberId + bookId + ".json", data);
+
+        let borrowData = r.read("./data/borrow/" + memberId + bookId + ".json");
+        let dat = new Date(borrowDate).getTime();
+        let due = dat + 1209600000;
+        borrowData.dueDate = new Date(due);
+
+        w.write("./data/borrow/" + memberId + bookId + ".json", borrowData);
       } else {
         console.log("cannot issue book to member cause of unavailable limit");
         throw new Error("Unavailable limit");
@@ -27,7 +34,7 @@ function borrowBook(memberId, bookId) {
     } else {
       a.add("./data/reserve/" + bookId + ".json", memberId);
       console.log("added member in reserve que");
-      throw error("book unavailable");
+      throw new Error("book unavailable");
     }
 
     console.log("book issued successfully");
--- borrowRecord.js
+++ borrowRecord.js
@@ -1,10 +1,9 @@
 class BorrowRecord {
-  constructor(memberId, bookId) {
+  constructor(memberId, bookId, borrowDate) {
     this.borrowId = memberId + bookId;
     this.memberId = memberId;
     this.bookId = bookId;
-    this.borrowDate = new Date();
-    this.dueDate = new Date(Date.now() + 1209600000);
+    this.borrowDate = new Date(borrowDate);
   }
 }
 
